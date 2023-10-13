ff32af12682af47188433716cc54eadcbbded108
Nicholas Buscemi <112017671+nickbuscemi@users.noreply.github.com>
2023-10-13 13:45:49 -0400

    finished assignment done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className='main'>
-      <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
-    </div>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/stocks" component={Stocks} />
+      <Route path="/Movies" component={Movies} />
+
+      <Route>
+        <h1>Page Not Found</h1>
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+// Creating the Root component
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+// Rendering the Root component
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
